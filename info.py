fb03ffcaa1a8819a62c0f21877bbd731c2a8eae1
Crazyboss1 <112222385+Crazyboss1@users.noreply.github.com>
2024-12-01 07:30:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,30 +5,25 @@ import re
 id_pattern = re.compile(r'^.\d+$')
 
 #API ID and HASH
-API_ID = int(os.getenv("API_ID", "22849062"))
-API_HASH = os.getenv("API_HASH", "e043bdee9d6f5a0061ea83d51655113f")
+API_ID = int(os.getenv("API_ID", "27348356"))
+API_HASH = os.getenv("API_HASH", "a6dc1dbe3feb1c263f749ad5f95ff445")
 
 #ADMINS
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6245128154').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6544671421 6484052423 7188908429').split()]
 
 #Bot token and User String 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7047759925:AAFH14m-13bfEhPYamEF2MsZimzjwtfkvcc")
-#appu
-USER_SESSION = os.getenv("USER_SESSION", "BAFcpiYAfXnuYkwHxwVxhjqkgEwhGpFXaUiM73OMU6d7HmhTdyBcAGujzhkUI3T_nHAF3fvZx2pG-0c35jxYK_xuO9w4T7nQt2x8Z75YIzYNs80bgyXLz0LidS-jpiQ2PjJ-X2Pufa-uxNo5zhq2swp-VjwM4NYLS6fSiIwu9x8ac0PyMaT2uevwLPR8H78CVD_iyH8J_nDgKSG8qKIwBQZdfU89d5wxREkBnACM6iG1lcJ8kRy8Cc7tpbDePrxZ-vb8PIpdNwyrU5SjYOT3N0pb057OjYnVM5OELiK2FxAisbxbyhyw58jG4ZlUcT7ScIDN9UIMH_NLSwzwCGHHKrRD6eVmqAAAAAF0PRfaAA")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6515902214:AAF5DnMU3LcrJpglXzrbW70BrQmrRisU6D0")
 
-#DB
-DB_URI = os.getenv("DB_URI", "mongodb+srv://mat:mat@cluster0.vtqpi4l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+USER_SESSION = os.getenv("USER_SESSION", "BQAf70YACd1OpLAfVtosIeoPCgpCPhW2ttPjOxytr1wGHilj7NV-mEWbArcjbE0XO6P2ofdBzP5KAL7jv_N6Td5dDr8GZtKGIFFZymo2G1FSWEdY4fA3s_Sbv616aJ6WfW7TepKK262AwPiipElm2Z5WFU33n-yCz4FnXNpLD-Q0vRefCp-KDe4oHrGydTid1Un1QwShDKN-tBouzz0cSv9XKmZqmthvzBKsbuvCe8FWa9KDqa-l2s38iNKGknBs4VpRw0WhP73nglvP4l89R1g3Ko7xcpHiycBuZgE5weErGIyAkTxz2p6wvCcyUY7-RnhvTFhl9sU5ehMZ57SP242NjkdG2AAAAAGGF8K9AA")
+
+DB_URI = os.getenv("DB_URI", "mongodb+srv://spm5:spm5@spm5.twy1d.mongodb.net/?retryWrites=true&w=majority&appName=Spm5")
 
 #LOG 
-LOG_CHANNEL = os.getenv("LOG_CHANNEL", "-1001668201351")
-LOG_MESSAGE = os.getenv("LOG_MESSAGE", "-1001946828338")
+LOG_CHANNEL = os.getenv("LOG_CHANNEL", "-1002018707320")
+LOG_MESSAGE = os.getenv("LOG_MESSAGE", "-1002220423739")
 
 #pm warn limit 
 WARN_LIMIT = os.getenv("WARN_LIMIT", "5")
 
 #COMMAND SUFIX
 CMD = ["*", ".", "!"]
-
-#AUTOPICZ 
-PICS_ID = os.getenv("PICS_ID", "-1001904493290")
-PIC_TIME = os.getenv("PIC_TIME", "600")
