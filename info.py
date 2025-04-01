9ce511105fb2a0a5ebf9951ffa6d0c78578042ab
Crazyboss1 <112222385+Crazyboss1@users.noreply.github.com>
2025-04-01 08:42:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,8 +15,7 @@ POSTERS = [int(poster) if id_pattern.search(poster) else poster for poster in os
 #Bot token and User String 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
-USER_SESSION = os.getenv("USER_SESSION", "BQAf70YAO7vzER8D2xYxwa5ONmK7FVhjt5SChdfZER-DrvG4OvnrqssHG6JDT2fzxiiYYVdOBHoWu9u4A8NzGTwhC9WDu8In0W4UGXEwINVRH-2U1OcGlobD81WP3RaBoKVqrVuqsTKf820yMqShVKvQmShFRmN4nMKQIpRjn5FUBBB7q84wJbrQDZMPfZnxv7RNNwDEKW60noS-hkVqtkSzLtDfbI4-f10r_PL4EoNjU-IySIij8WFqVwPiE6K6uaE9e2wcjVq4fk0kOY4HrbVyVPtXhVI0tH4oGN2Tnzrk4bHzh5WpAMf5tIlu5hU6B1mtPPYQ5fn3zFrX0dgIj55LLmRzMQAAAAGsfgmNAA")
-
+USER_SESSION = os.getenv("USER_SESSION", "")
 DB_URI = os.getenv("DB_URI", "")
 
 #LOG 
